214bc0d17bbfd600c2a3bfbff88d89b027c920ec
Sturmel Sébastien <sturmel.sebastien@gmail.com>
2025-04-12 22:58:21 +0200

    enhance security and performance by adding CSP policies, optimizing build settings, and enabling asset compression

--- html/nuxt.config.ts
+++ html/nuxt.config.ts
@@ -16,6 +16,37 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
+  csp: {
+    hashAlgorithm: 'sha256', 
+    policies: {
+      'default-src': ["'self'", 'https://lugh-web.fr'],
+      'script-src': ["'self'", "'unsafe-inline'", 'https://lugh-web.fr', 'https://client.axept.io', 'https://client.crisp.chat'  ],
+      'style-src': ["'self'", "'unsafe-inline'"],
+      'img-src': ["'self'", 'data:'],
+    },
+    addMeta: true, 
+  },
+  nitro: {
+    compressPublicAssets: true,
+    prerender: {
+      crawlLinks: true   
+    }
+  },
+  build: {
+    extractCSS: true,
+    splitChunks: {
+      layouts: true,
+      pages: true,
+      commons: true
+    },
+    terser: {
+      terserOptions: {
+        compress: {
+          drop_console: true,
+        }
+      }
+    }
+  },
   particles: {
     mode: 'slim',
     lazy: true
