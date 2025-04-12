08d2ef8265ade69f679d6170a6dd1bae54effc62
Sturmel Sébastien <sturmel.sebastien@gmail.com>
2025-04-12 23:03:29 +0200

    remove unused Nuxt modules and configurations for cleaner setup

--- html/nuxt.config.ts
+++ html/nuxt.config.ts
@@ -4,10 +4,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [    
-    '@nuxt/eslint',
+    //'@nuxt/eslint',
     '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/test-utils/module',
+    //'@nuxt/image',
+    //'@nuxt/test-utils/module',
     'nuxt-particles'
   ],
   css: ['~/assets/style/main.css'],
@@ -16,23 +16,14 @@ export default defineNuxtConfig({
       tailwindcss(),
     ],
   },
-  nitro: {
-    compressPublicAssets: true,
-    prerender: {
-      crawlLinks: true   
-    }
-  },
   particles: {
     mode: 'slim',
     lazy: true
   },
   icon: {
     serverBundle: {
-      collections: ['ph', 'mdi'],
+      collections: ['mdi'],
     }
-  },
-  image: {
-    format: ['avif', 'webp', 'jpg', 'png'],
   },
   runtimeConfig: {  
     webhookUrl: process.env.WEBHOOK_URL
