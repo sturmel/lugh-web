717e410704e07d2a5f26367e3ef892523e26c1ac
Sturmel Sébastien <sturmel.sebastien@gmail.com>
2025-04-18 10:03:00 +0200

    update Content Security Policy to allow Google Tag Manager and Google Analytics connections

--- html/app.vue
+++ html/app.vue
@@ -7,17 +7,17 @@
       <Meta name="googlebot" content="index, follow" />
 
       <!-- Security Headers -->
-      <Meta http-equiv="Content-Security-Policy" content="
-    default-src 'self';
-    script-src 'self' 'unsafe-inline';
-    style-src 'self' fonts.googleapis.com 'unsafe-inline';
-    img-src 'self' data:;
-    font-src 'self' fonts.gstatic.com;
-    connect-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-  " />
+      <Meta http-equiv="Content-Security-Policy"         content="
+          default-src 'self';
+          script-src 'self' 'unsafe-inline' https://www.googletagmanager.com;
+          style-src 'self' fonts.googleapis.com 'unsafe-inline';
+          img-src 'self' data:;
+          font-src 'self' fonts.gstatic.com;
+          connect-src 'self' https://*.google-analytics.com https://*.analytics.google.com;
+          object-src 'none';
+          base-uri 'self';
+          form-action 'self';
+        " />
       <Meta http-equiv="X-Content-Type-Options" content="nosniff" />
       <Meta name="referrer-policy" content="strict-origin-when-cross-origin" />
       <Meta http-equiv="Permissions-Policy" content="geolocation=(), camera=(), microphone=(), display-capture=()" />
